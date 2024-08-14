89698bdbd9c93e59e751c676b94a4abac1f2b5fc
Dmitry V <myredirectemail@gmail.com>
2024-08-14 04:26:28 +0300

    сериализация в файл

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,28 @@ import display.ConsoleMenu;
 import entity.AnimalModel;
 import entity.BarrelModel;
 import entity.PersonModel;
+import service.serialization.FileSerialization;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        AnimalModel cat = AnimalModel.builder()
-//                .species("Cat")
-//                .eyeColor("Green")
-//                .hasFur(true)
-//                .build();
-//
+        AnimalModel cat = AnimalModel.builder()
+                .species("Cat")
+                .eyeColor("Green")
+                .hasFur(true)
+                .build();
+        AnimalModel dog = AnimalModel.builder().species("Dog")
+                .eyeColor("Brown")
+                .hasFur(true)
+                .build();
+        AnimalModel axolotl = AnimalModel.builder().species("Axolotl")
+                .eyeColor("Gray")
+                .hasFur(false)
+                .build();
+
+        var animals = new AnimalModel[]{ cat, dog, axolotl };
+
 //        BarrelModel waterBarrel = BarrelModel.builder()
 //                .volume(50.0)
 //                .storedMaterial("Water")
@@ -26,8 +39,9 @@ public class Main {
 //        System.out.println(cat);
 //        System.out.println(waterBarrel);
 //        System.out.println(person);
-
-        new ConsoleMenu().start();
+        FileSerialization<AnimalModel> ser = new FileSerialization<>();
+        ser.WriteObjectsToFile("asdf.txt", Arrays.asList(animals));
+        //new ConsoleMenu().start();
 
     }
 }
--- src/main/java/service/serialization/FileSerialization.java
+++ src/main/java/service/serialization/FileSerialization.java
@@ -1,4 +1,37 @@
 package service.serialization;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class FileSerialization {
+import java.io.*;
+import java.lang.String;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSerialization<T> {
+
+    public List<T> GetObjectsFromFile(String path){
+        Gson gson = new Gson();
+
+        List<T> list = new ArrayList<>();
+        try (FileInputStream fin = new FileInputStream(path); ObjectInputStream oos = new ObjectInputStream(fin);){
+            var content = Files.readString(Paths.get(path));
+            list = gson.fromJson(content, TypeToken.get(list.getClass()).getType());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    public void WriteObjectsToFile(String path, List<T> list){
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        try {
+            Files.write(Paths.get(path), json.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
