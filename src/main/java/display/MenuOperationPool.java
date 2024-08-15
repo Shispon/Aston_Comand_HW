162aded41f758fe2c105c8f5c2dad2b1bbeaa677
Mikael_Yatsenko <mikael.yatsenko@yandex.ru>
2024-08-15 21:13:58 +0400

    adds operation pool

---
+++ src/main/java/display/MenuOperationPool.java
@@ -0,0 +1,27 @@
+package display;
+
+public class MenuOperationPool {
+    public static void manualInputSorting() {
+        System.out.println("Вы выбрали Подопцию 1.1");
+    }
+
+    public static void manualInputSearch() {
+        System.out.println("Вы выбрали Подопцию 1.2");
+    }
+
+    public static void randomInputSorting() {
+        System.out.println("Вы выбрали Подопцию 2.1");
+    }
+
+    public static void randomInputSearch() {
+        System.out.println("Вы выбрали Подопцию 2.2");
+    }
+
+    public static void fileInputSorting() {
+        System.out.println("Вы выбрали Подопцию 3.1");
+    }
+
+    public static void fileInputSearch() {
+        System.out.println("Вы выбрали Подопцию 3.2");
+    }
+}
