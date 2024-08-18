656cd227c42c124ac2eabc091b7a6c86da13e463
Shispon <138853327+Shispon@users.noreply.github.com>
2024-08-18 23:49:43 +0300

    Добавил доп задание по соритировке Добавил javadock в свои репозитории и новый метод сортировки в два из 3 Изменил Тест сортировок

--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -14,13 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Репозиторий для работы с коллекцией людей.
+ * Предоставляет функциональность для сортировки, поиска, добавления и загрузки людей.
+ */
 public class PersonRepository {
+
     private final List<PersonModel> personList = new ArrayList<>();
     private final ConsoleSerialization<PersonModel> consoleSerialization;
     private final FileSerialization<PersonModel> fileSerialization;
     private final RandomSerialization<PersonModel> randomSerialization;
 
-    // Конструктор для инициализации зависимостей
+    /**
+     * Конструктор класса {@code PersonRepository}.
+     *
+     * @param consoleSerialization объект для сериализации в консоль
+     * @param fileSerialization объект для сериализации в файл
+     * @param randomSerialization объект для случайной сериализации
+     */
     public PersonRepository(ConsoleSerialization<PersonModel> consoleSerialization,
                             FileSerialization<PersonModel> fileSerialization,
                             RandomSerialization<PersonModel> randomSerialization) {
@@ -29,17 +40,36 @@ public class PersonRepository {
         this.randomSerialization = randomSerialization;
     }
 
-    // Метод для сортировки списка PersonModel
+    /**
+     * Сортирует список людей с использованием стандартной сортировки.
+     */
     public void sortedPersonList() {
         SortingService.insertionSort(personList, Comparators.personComparator());
     }
 
-    // Метод для поиска PersonModel в списке
+    /**
+     * Специально сортирует список людей, где сортируются только те люди,
+     * возраст которых является четным, а затем возвращаются в их исходные позиции.
+     */
+    public void specialSortedPersonList() {
+        SortingService.specialSort(personList, Comparators.personComparator(), PersonModel::getAge);
+    }
+
+    /**
+     * Ищет человека в списке по указанным критериям.
+     *
+     * @param person человек для поиска
+     * @return {@code true}, если человек найден, иначе {@code false}
+     */
     public boolean searchPerson(PersonModel person) {
         return BinarySearchService.search(personList, person, Comparators.personComparator()) != null;
     }
 
-    // Метод для добавления PersonModel через консольный ввод
+    /**
+     * Добавляет человека в список на основе ввода с консоли.
+     *
+     * @throws ValidationException если возникла ошибка при валидации данных
+     */
     public void addPersonByConsole() throws ValidationException {
         Scanner scanner = new Scanner(System.in);
         PersonModel person = new PersonModel();
@@ -65,19 +95,28 @@ public class PersonRepository {
         }
     }
 
-    // Метод для получения PersonModel из файла
+    /**
+     * Загружает людей из файла и обновляет список людей.
+     */
     public void getPersonsFromFile() {
         List<PersonModel> personsFromFile = fileSerialization.GetObjectsFromFile("persons.json", PersonModel.class);
         personList.clear();
         personList.addAll(personsFromFile);
     }
 
-    // Метод для добавления PersonModel в файл
+    /**
+     * Записывает список людей в файл.
+     */
     public void addPersonsByFile() {
         fileSerialization.WriteObjectsToFile("persons.json", personList);
     }
 
-    // Метод для добавления случайных PersonModel
+    /**
+     * Добавляет случайно сгенерированных людей в список.
+     *
+     * @param count количество людей для генерации
+     * @throws RuntimeException если возникает ошибка при генерации случайных людей
+     */
     public void addPersonsByRandom(int count) {
         try {
             List<PersonModel> randomPersons = randomSerialization.GetRandomObjects(count, PersonModel.class);
@@ -87,7 +126,10 @@ public class PersonRepository {
         }
     }
 
-    public void getAllPerson () {
+    /**
+     * Выводит все люди из списка на консоль.
+     */
+    public void getAllPerson() {
         System.out.println(personList);
     }
 }
